aebf54ec871366a5cd965e6a306535366db79eab
ibrahimshamma99 <i.abushammah@gmail.com>
2021-12-07 18:26:52 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -6,17 +6,7 @@ const abstractSim = (s1, s2) => {
     f: similarityScore.dl, //winklerMetaphone , metaphoneDl , dl , commonScore
     options: { threshold: 0.6 },
   };
-  // const sim1 = similarity(s1, s2, winkOpts);
-  // const sim2 = similarity(s2, s1, winkOpts);
-  // if (sim1.score >= sim2.score) {
-  //   return sim1;
-  // } else {
-  //   return sim2;
-  // }
   return similarity(s1, s2, winkOpts);
 };
 
 module.exports = abstractSim;
-
-// console.log(abstractSim(["فجل"], ["الفجل"]).matchScore);
-// console.log(abstractSim(["الفجل"], ["فجل"]).matchScore);
